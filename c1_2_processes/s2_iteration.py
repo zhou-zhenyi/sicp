622c3f4eb1c1a28db6032f1ef67d3a33112f637f
Zhou <zhenyi.zhou@sap.com>
2019-11-26 17:42:29 +0800

    Chapter 1.2 commit

---
+++ c1_2_processes/s2_iteration.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    return fact_iter(1, 1, n)
+
+def fact_iter(product, counter, max_count):
+    if counter > max_count:
+        return product
+    else:
+        return fact_iter(counter * product, counter + 1, max_count)
+
+print(factorial(6))
