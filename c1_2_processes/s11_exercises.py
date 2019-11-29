fc6c173ad40b7664525b46ace1d3728fc1ebe1f3
Zhou <zhenyi.zhou@sap.com>
2019-11-29 18:05:13 +0800

    Commit adjustments

--- c1_2_processes/s11_exercises.py
+++ c1_2_processes/s11_exercises.py
@@ -1,11 +1,35 @@
 from util import logl
+from util import newl
 
-def multiply(a, b):
+def expt_mult(a, b):
     logl("(" + str(a) + ", " + str(b) + ")")
 
     if b == 0:
         return 0
     else:
-        return a + multiply(a, b - 1)
+        return a + expt_mult(a, b - 1)
 
-print(multiply(3, 5))
+print(expt_mult(3, 9))
+
+newl()
+
+def double(x):
+    return x + x
+
+def halve(x):
+    return x / 2
+
+def even(n):
+    return n % 2 == 0
+
+def fast_expt(a, b):
+    logl("(" + str(a) + ", " + str(b) + ")")
+
+    if b == 0:
+        return 0
+    elif even(b):
+        return double(fast_expt(a, halve(b)))
+    else:
+        return a + fast_expt(a, b - 1)
+
+print(fast_expt(3, 9))
