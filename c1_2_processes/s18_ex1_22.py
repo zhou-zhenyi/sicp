2d53b2a565031f2fb6ab71ccd9383b5619a1236d
Zhou <zhenyi.zhou@sap.com>
2019-12-02 18:07:41 +0800

    Finish ex1.22

--- c1_2_processes/s18_ex1_22.py
+++ c1_2_processes/s18_ex1_22.py
@@ -1,19 +1,24 @@
-from time import time
+import sys
+sys.setrecursionlimit(10000000)
+
+from time import time_ns
 
 from util import square
+from util import even
 
 def timed_prime_test(n):
-    print("\n")
-    print(n)
-    start_prime_test(n, time())
+    return start_prime_test(n, time_ns())
 
 def start_prime_test(n, start_time):
-    if prime(n):
-        return report_prime(time() - start_time)
+    is_prime = prime(n)
 
-def report_prime(elapsed_time):
-    print(" *** ")
-    print(elapsed_time)
+    if is_prime:
+        report_prime(n, time_ns() - start_time)
+
+    return is_prime
+
+def report_prime(n, elapsed_time):
+    print(str(n) + " *** " + str(elapsed_time))
 
 def prime(n):
     def smallest_divisor(n):
@@ -32,4 +37,20 @@ def prime(n):
 
     return n == smallest_divisor(n)
 
-timed_prime_test(113)
+def search_for_prime(n, i):
+    if i == 0:
+        return
+    elif even(n):
+        n += 1
+    else:
+        n += 2
+
+    if timed_prime_test(n):
+        i -= 1
+
+    search_for_prime(n, i)
+
+search_for_prime(1000, 3)
+search_for_prime(10000, 3)
+search_for_prime(100000, 3)
+search_for_prime(1000000, 3)
