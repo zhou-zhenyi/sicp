97d3f5b8cba80bfca9bfd8b1a9f32da86745ea00
zhou-zhenyi <zhenyi.zhou@hotmail.com>
2020-02-08 19:18:11 +0800

    s1修改

--- c2_2_closure/s1_sequence.py
+++ c2_2_closure/s1_sequence.py
@@ -3,5 +3,10 @@ from util_pair import cons, car, cdr, list, print_list
 x = cons(1, cons(2, cons(3, cons(4, None))))
 print_list(x)
 
-y = list([1, 2, 3, 4])
-print_list(y)
+one_through_four = list([1, 2, 3, 4])
+print_list(one_through_four)
+print(car(one_through_four))
+print_list(cdr(one_through_four))
+print(car(cdr(one_through_four)))
+print_list(cons(10, one_through_four))
+print_list(cons(5, one_through_four))
